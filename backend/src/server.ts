4b89c5a0b816c1cb0bf917f10d70ff8e80b3d524
Ryan Pavini <r236799@dac.unicamp.br>
2025-11-14 20:49:17 -0300

    fix(backend): Corrigindo server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
@@ -6,20 +7,21 @@ import router from './routes';
 import apiKeyMiddleware from './middlewares/apiKeyMiddleware';
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL, 
+  origin: process.env.FRONTEND_URL,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type, Authorization, x-api-key"
 };
 
-
 const app = express();
+const PORT = 3333;
 
-app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));
 
-app.use(cors(corsOptions)); 
-app.use(express.json());   
-app.use(apiKeyMiddleware); 
+app.use(express.json());
+app.use(apiKeyMiddleware);
 
-app.use(router); 
+app.use(router);
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
